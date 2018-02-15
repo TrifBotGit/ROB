fc0e9b18843c2e3b69973929a4d789dfe9e79d30
TrifBotGit <a15danrivcar@iam.cat>
2018-02-15 11:32:53 +0100

    Something

--- TestBot/TrifulBot/src/main/java/bot/trifulca/TrifulBot/Utils/CommandHandling/lolcommands/LeagueHandling.java
+++ TestBot/TrifulBot/src/main/java/bot/trifulca/TrifulBot/Utils/CommandHandling/lolcommands/LeagueHandling.java
@@ -2,6 +2,8 @@ package bot.trifulca.TrifulBot.Utils.CommandHandling.lolcommands;
 
 public class LeagueHandling {
 	
+	private final String GGAPITOKEN = "f4acd1426c67115e9ec9cf73615bdaa9";
+	
 	
 
 }
--- TestBot/TrifulBot/src/main/java/bot/trifulca/TrifulBot/Utils/MyEvents.java
+++ TestBot/TrifulBot/src/main/java/bot/trifulca/TrifulBot/Utils/MyEvents.java
@@ -8,8 +8,22 @@ public class MyEvents {
 	
 	@EventSubscriber
     public void onMessageReceived(MessageReceivedEvent event){
-        if(event.getMessage().getContent().startsWith(BotUtils.BOT_PREFIX)&&!event.getAuthor().isBot())
-            CommandHandler.handleCommand(event.getMessage().getContent());
+        if(event.getMessage().getContent().startsWith(BotUtils.BOT_PREFIX)&&!event.getAuthor().isBot()){
+        	if(event.getMessage().getContent().startsWith("!game")){
+        		gameCommandReceived(event);
+        	}else{
+        		musicCommandReceived(event);
+        	}
+        } 	
     }
+	
+	public void gameCommandReceived(MessageReceivedEvent event){
+		String response = CommandHandler.handleCommand(event.getMessage().getContent());
+		BotUtils.sendMessage(event.getChannel(), response);
+	}
+	
+	public void musicCommandReceived(MessageReceivedEvent event){
+		
+	}
 
 }
